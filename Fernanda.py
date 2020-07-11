503622f02bd06ffb184cbf859adfe1597371805b
leo-885 <65372884+leo-885@users.noreply.github.com>
2020-07-11 16:01:31 -0300

    Add files via upload

---
+++ Fernanda.py
@@ -0,0 +1,9 @@
+#aprendendo if... else
+
+num =	float(input('      Fernanda é apaixonada por Pablo, seu colega de classe, e por isso quer se casar com ele mas, a sua mãe diz que é   preciso esperar um pouco mais... Se Ana Clara, a irmã mais  velha (que tem um gato chamado Lucrécio) tem 25 anos de     idade, Amanda é 4 anos mais nova e Fernanda nasceu 10 anos  depois de Ana Clara, Fernanda tem ainda de esperar quantos  anos para se casar no Brasil? \n\n    '))
+
+if num  ==	3:
+	    print("        Acertou!")
+if num !=	3:	
+        print("        tá meio errado mano")
+           
